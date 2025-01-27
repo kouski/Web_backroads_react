8d31405ecae4de074dfa400989b1ca53114477a8
kouski <72550619+kouski@users.noreply.github.com>
2025-01-27 12:16:34 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import About from "./components/About"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import Services from "./components/Services"
+import Tours from "./components/Tours"
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Header />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+      
+    </>
+  )
+}
+
+export default App
